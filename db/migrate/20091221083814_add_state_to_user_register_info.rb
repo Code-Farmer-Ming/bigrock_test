54d073c57d0adec11c5ddc8826917b1a96a30416
mzhang <zhangming12@hotmail.com>
2010-01-25 20:55:32 +0800

    change migrate/20091221083814_add_state_to_user_register_info.rb

--- db/migrate/20091221083814_add_state_to_user_register_info.rb
+++ db/migrate/20091221083814_add_state_to_user_register_info.rb
@@ -1,6 +1,6 @@
 class AddStateToUserRegisterInfo < ActiveRecord::Migration
   def self.up
-    add_column :user_register_infos, :state, :string,:limit=>12,:default=>UserRegisterInfo::STATE_TYPES.keys[0].to_s()
+    add_column :user_register_infos, :state, :string,:limit=>12,:default=>User::STATE_TYPES.keys[0].to_s()
     remove_column :resumes, :state
   end
 
