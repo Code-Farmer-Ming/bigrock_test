7cd401b4878b780e54f565c6a60a5ba3c8ae287d
mzhang <zhangming12@hotmail.com>
2011-09-09 18:24:32 +0800

    修改者关注小组 显示的问题

--- db/migrate/20110909101339_re_attention_group.rb
+++ db/migrate/20110909101339_re_attention_group.rb
@@ -1,11 +1,16 @@
-class AttentionGroup < ActiveRecord::Migration
+class ReAttentionGroup < ActiveRecord::Migration
   def self.up
+    User.all.each() do |user|
+      user.all_groups.each() do |group|
+        user.remove_attention(group)
+      end
+    end
+    
     User.real_users.each() do |user|
       user.all_groups.each() do |group|
         user.add_attention(group)
       end
     end
-
   end
 
   def self.down
