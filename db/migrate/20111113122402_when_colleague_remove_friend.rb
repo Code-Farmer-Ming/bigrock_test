5a009a68dc05070b123fb2785ee6a809e145d445
mzhang <zhangming12@hotmail.com>
2011-11-13 21:18:19 +0800

    数据合并时 把发现 有colleague 把friend的身份去除

--- db/migrate/20111113122402_when_colleague_remove_friend.rb
+++ db/migrate/20111113122402_when_colleague_remove_friend.rb
@@ -1,6 +1,14 @@
 
 class WhenColleagueRemoveFriend < ActiveRecord::Migration
   def self.up
+    User.real_users.each do |user|
+      user.colleague_users do |colleague|
+        user.friend_users.delete(colleague)
+        colleague.friend_users.delete(user)
+        colleague.my_add_friend_application_users.delete(user)
+        user.my_add_friend_application_users.delete(colleague)
+      end
+    end
   end
 
   def self.down
